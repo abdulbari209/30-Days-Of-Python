579efbb5bdd3009f23355417024062b9ca8b675b
Abdulbari <60986760+abdulbari209@users.noreply.github.com>
2024-01-10 01:17:26 +0500

    Update and rename variable.py to variables.py

---
+++ 02_Day_Variables_builtin_functions/variables.py
@@ -0,0 +1,55 @@
+# Day 2: 30 Days of python programming
+
+# Level 1
+first_name = 'abdul'
+last_name = 'bari'
+full_name = first_name + ' ' + last_name
+country = 'pakistan'
+city = 'karachi'
+age = 23
+year = 2024
+is_married = False
+is_true = True
+is_light_on = True
+language, email = 'urdu', 'abdulbarimehmood200@gmail.com'
+
+# Level 2
+print(type(first_name))
+print(type(last_name))
+print(type(full_name))
+print(type(country))
+print(type(city))
+print(type(age))
+print(type(year))
+print(type(is_married))
+print(type(is_true))
+print(type(is_light_on))
+print(type(language))
+print(type(email))
+
+print(len(first_name))
+
+print(len(first_name) > len(last_name))
+
+num_one = 5
+num_two = 4
+total = num_one + num_two
+diff = num_one - num_two
+product = num_one * num_two
+division = num_one / num_two
+remainder = num_two % num_one
+exp = num_one ** num_two
+floor_division = num_one // num_two
+
+radius = int(input('Enter radius:'))
+area_of_circle = 3.14*radius*radius
+circumference_of_circle = 3.14 * 2 * radius
+print(area_of_circle)
+print(circumference_of_circle)
+
+first = input('Enter first name:')
+last = input('Enter last name:')
+country = input('Enter country:')
+age = int(input('Enter age:'))
+
+help('keywords')
